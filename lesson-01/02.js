db56a11768cff41b11320a41a39221b5c8de17db
Maximaab <141754764+Maximaab@users.noreply.github.com>
2023-10-14 15:07:55 +0300

    commit Chet Nechet1

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,6 +1,7 @@
 // code
 let number1 = Math.floor(Math.random() * 100)
-if (number1 % 2 ) {
-    alert ("Это четное число")
-} else alert ("Это нечетное число")
-console.log(number1)
+// if (number1 % 2 ) {
+//     alert ("Это четное число")
+// } else alert ("Это нечетное число")
+// console.log(number1)
+number1 % 2 ? console.log("Это четное число"):console.log("Это нечетное число")
