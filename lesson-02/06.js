baf1f05cd5fbf5a9761447425e5a33b744b50334
Maximaab <141754764+Maximaab@users.noreply.github.com>
2023-10-19 16:46:35 +0300

    commit 02.06

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -8,4 +8,5 @@ let passportWithAddress = {
 };
 let copyPassportWithAddress = {...passportWithAddress, address: {...passportWithAddress.address, city :"Bobryisk"}}
 
-
+console.log(passportWithAddress.address.city)
+console.log(copyPassportWithAddress.address.city)
