9e3e11c545c1a8b7b2db231cc3decc7a142eb719
Maximaab <141754764+Maximaab@users.noreply.github.com>
2023-10-19 16:25:14 +0300

    commit 02.05

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -1,10 +1,10 @@
 let passport = {
     name: "Petr",
-
+    surname: "Petrov",
 };
 let copyPassport = passport
 let copuPassport = {
     name:"Ivan"
 }
-console.log(passport)
-console.log(copuPassport)
+console.log(passport.name)
+console.log(copuPassport.name)
