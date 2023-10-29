8b36a9a89d6d31df2b69d6767d6b14983e7037f1
Maximaab <141754764+Maximaab@users.noreply.github.com>
2023-10-29 18:08:42 +0200

    commit 03.03

--- lesson-03/07.js
+++ lesson-03/07.js
@@ -3,7 +3,7 @@ const numbers = [
     [11, 20, 33, 40, 55],
     [111, 200, 333, 400, 555],
 ];
-console.log(numbers.map(el=>el.filter()))
+console.log(numbers.map(el=>el.filter(el=>el % 2 ===0)))
 
 
 
