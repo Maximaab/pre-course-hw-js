59624653de64528dac319d14a47fff8cbf7e7196
Maximaab <141754764+Maximaab@users.noreply.github.com>
2023-10-29 18:04:51 +0200

    commit 03.03

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,12 +1,18 @@
 const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
-const NewWords = (words) => {
-    if(words.indexOf("апельсин")) {
-        return  "Ура! нашел"
-    } else {
-         return "Придется поискать в другом магазине…"
-    }
+const newWords = (words) => {
+
+    const searchIndex = words.indexOf("апельсин")
+
+    if (searchIndex === -1) return  "Придется поискать в другом магазине…"
+    return "Ура! нашел"
+
+    // if(words.indexOf("апельсин")) {
+    //     return  "Ура! нашел"
+    // } else {
+    //      return "Придется поискать в другом магазине…"
+    // }
 }
-console.log(NewWords(words))
+console.log(newWords(words))
 
 
 
